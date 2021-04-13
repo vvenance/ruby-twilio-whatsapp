91f828e8d37710f71ff391f71328d95c4364cd4f
Valériane Venance <valeriane.venance@hotmail.fr>
2021-04-13 17:14:26 +0200

    Add media url

--- whatsapp-sender.rb
+++ whatsapp-sender.rb
@@ -6,5 +6,6 @@ client = Twilio::REST::Client.new(account_sid, auth_token)
  
 message = client.messages.create(from: 'whatsapp:+14155238886',
                                  to: 'whatsapp:+<your whatsapp phone number>',
-                                 body: 'Ahoy WhatsApp user!'
+                                 body: 'Ahoy WhatsApp user!',
+                                 media_url: 'https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg?cs=srgb&dl=pexels-dominika-roseclay-2023384.jpg&fm=jpg'
                                 )
